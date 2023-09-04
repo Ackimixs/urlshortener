a7da2510ad4e6a28b63fe3d23b950098e265388e
Ackimixs_ <ackimixs@protonmail.com>
2023-09-05 01:35:05 +0200

    update

--- layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="bg-neutral-800 text-white h-full px-10 py-5">
-    <slot />
-  </div>
-</template>
--- prisma/migrations/20230823161835_add_url/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "url" (
-    "id" TEXT NOT NULL,
-    "OriginUrl" TEXT NOT NULL,
-
-    CONSTRAINT "url_pkey" PRIMARY KEY ("id")
-);
