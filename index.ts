8132da03eb388b0f8c1170acb1fac86a4e97c67b
Ackimixs_ <ackimixs@protonmail.com>
2023-10-01 21:35:58 +0200

    up

--- index.ts
+++ index.ts
@@ -4,70 +4,145 @@ import { PrismaClient } from "@prisma/client";
 const app = server();
 const prisma = new PrismaClient();
 
+app.get("/api/url", async (req, res) => {
+  console.log("GET /api/url");
 
-app.get('/api/url', async (req, res) => {
-    console.log('GET /api/url')
+  const url = await prisma.url.findMany();
 
-    const url = await prisma.url.findMany();
-
-    res.json({ body: {url} });
-})
-
-app.post('/api/url', async (req, res) => {
-    console.log('POST /api/url')
-
-    const { code, long_url } = req.body;
-
-    if (!isUrl(long_url)) return res.json({ body: {error: 'Invalid URL'} });
-
-    const url = await prisma.url.create({
-        data: {
-            code,
-            long_url
-        }
-    })
-
-    res.json({ body: {url} });
-})
-
-app.get('/', (req, res) => {
-    console.log('GET /')
-
-    res.send(Bun.file('./public/index.html'));
-})
-
-app.get('/r/:id', async (req, res) => {
-    console.log('GET /r/:id')
-
-    const { id } = req.params;
-
-    const url = await prisma.url.findUnique({
-        where: {
-            code: id
-        }
-    });
-
-    if (url) {
-        res.headers({
-            'Location': url.long_url
-        }).status(301).send('Redirecting...')
-    } else {
-        res.headers({
-            'Location': '/'
-        }).status(404).send('Not found')
-    }
-})
-
-app.listen(3000, () => {
-    console.log('Server listening on port 3000')
-    prisma.$connect();
+  res.json({ body: { url } });
 });
 
-const isUrl = (url: string) : boolean => {
-    try {
-        new URL(url);
-        return true;
-    } catch (err) {
-        return false;
-    }
-}
+app.post("/api/url", async (req, res) => {
+  console.log("POST /api/url");
+
+  const { code, long_url } = req.body as { code: string; long_url: string };
+
+  if (!isUrl(long_url)) return res.json({ body: { error: "Invalid URL" } });
+
+  const url = await prisma.url.create({
+    data: {
+      code,
+      long_url,
+    },
+  });
+
+  res.json({ body: { url } });
+});
+
+app.get("/", (req, res) => {
+  console.log("GET /");
+
+  res.send(Bun.file("./public/index.html"));
+});
+
+app.get("/r/:id", async (req, res) => {
+  console.log("GET /r/:id");
+
+  const { id } = req.params as { id: string };
+
+  const url = await prisma.url.findUnique({
+    where: {
+      code: id,
+    },
+  });
+
+  if (url) {
+    res
+      .headers({
+        Location: url.long_url,
+      })
+      .status(301)
+      .send("Redirecting...");
+  } else {
+    res
+      .headers({
+        Location: "/",
+      })
+      .status(404)
+      .send("Not found");
+  }
+});
+
+app.delete("/api/url/:id", async (req, res) => {
+  console.log("DELETE /api/url/:id");
+
+  const { id } = req.params as { id: string };
+
+  const url = await prisma.url.delete({
+    where: {
+      id,
+    },
+  });
+
+  res.json({ body: { url } });
+});
+
+app.put("/api/url/:id", async (req, res) => {
+  console.log("PUT /api/url/:id");
+
+  const { id } = req.params as { id: string };
+  const { code, long_url } = req.body as { code: string; long_url: string };
+
+  if (!isUrl(long_url)) return res.json({ body: { error: "Invalid URL" } });
+
+  const url = await prisma.url.update({
+    where: {
+      id,
+    },
+    data: {
+      code,
+      long_url,
+    },
+  });
+
+  res.json({ body: { url } });
+});
+
+app.patch("/api/url/:id", async (req, res) => {
+  console.log("PATCH /api/url/:id");
+
+  const { id } = req.params as { id: string };
+  const { code, long_url } = req.body as { code: string; long_url: string };
+
+  if (!isUrl(long_url)) return res.json({ body: { error: "Invalid URL" } });
+
+  const url = await prisma.url.update({
+    where: {
+      id,
+    },
+    data: {
+      code,
+      long_url,
+    },
+  });
+
+  res.json({ body: { url } });
+});
+
+app.get("/api/url/:id", async (req, res) => {
+  console.log("GET /api/url/:id");
+
+  const { id } = req.params as { id: string };
+
+  const url = await prisma.url.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  res.json({ body: { url } });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+  prisma.$connect();
+});
+
+const isUrl = (url: string): boolean => {
+  try {
+    new URL(url);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
