ca44e6d32089a1a2e2e533ad1e0d7b3611ab0375
Ackimixs_ <ackimixs@protonmail.com>
2023-10-01 23:15:58 +0200

    up

--- main.cpp
+++ main.cpp
@@ -3,35 +3,35 @@
 
 int main()
 {
-    crow::SimpleApp app; //define your crow application
+    crow::SimpleApp app; // define your crow application
 
     app.loglevel(crow::LogLevel::INFO);
 
-    try {
+    try
+    {
         // Open a database file in create/write mode
-        SQLite::Database db("test.db3", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
+        SQLite::Database db("test.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
         std::cout << "SQLite database file '" << db.getFilename().c_str() << "' opened successfully\n";
 
-        SQLite::Statement query(db, "CREATE TABLE IF NOT EXISTS urls (code TEXT, long_url TEXT)");
+        SQLite::Statement query(db, "CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY AUTOINCREMENT, code TEXT, long_url TEXT)");
         query.exec();
         std::cout << "SQLite table 'urls' created successfully\n";
-
-    } catch (std::exception& e)
+    }
+    catch (std::exception &e)
     {
         std::cout << "SQLite exception: " << e.what() << std::endl;
         return EXIT_FAILURE; // unexpected error : exit the example program
     }
 
-
-
-    //define your endpoint at the root directory
-    CROW_ROUTE(app, "/")([](){
+    // define your endpoint at the root directory
+    CROW_ROUTE(app, "/")
+    ([]()
+     {
         auto page = crow::mustache::load_text("index.html");
-        return page;
-    });
+        return page; });
 
-    CROW_ROUTE(app, "/api/url").methods(crow::HTTPMethod::GET)([](const crow::request& req)
-        {
+    CROW_ROUTE(app, "/api/url").methods(crow::HTTPMethod::GET)([](const crow::request &req)
+                                                               {
             auto param = req.url_params;
             auto code = param.get("code");
             auto long_url = param.get("long_url");
@@ -85,11 +85,10 @@ int main()
                     i++;
                 }
                 return crow::response(x);
-            }
-        });
+            } });
 
-    CROW_ROUTE(app, "/api/url").methods(crow::HTTPMethod::POST)([](const crow::request& req)
-        {
+    CROW_ROUTE(app, "/api/url").methods(crow::HTTPMethod::POST)([](const crow::request &req)
+                                                                {
             auto x = crow::json::load(req.body);
             if (!x)
                 return crow::response(400);
@@ -114,10 +113,10 @@ int main()
                 return crow::response(y);
             }
 
-            return crow::response(400);
-        });
+            return crow::response(400); });
 
-    CROW_ROUTE(app, "/<string>").methods(crow::HTTPMethod::GET)([](const std::string code) {
+    CROW_ROUTE(app, "/r/<string>").methods(crow::HTTPMethod::GET)([](const std::string code)
+                                                                  {
         SQLite::Database db("test.db3", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
         SQLite::Statement query(db, "SELECT * FROM urls WHERE code = ?");
         query.bind(1, code);
@@ -135,13 +134,63 @@ int main()
             crow::response res;
             res.redirect_perm(long_url);
             return res;
+        } });
+
+    CROW_ROUTE(app, "/api/url/<string>").methods(crow::HTTPMethod::DELETE)([](const std::string code)
+                                                                           {
+        SQLite::Database db("test.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+        SQLite::Statement query(db, "DELETE FROM urls WHERE code = ?");
+        query.bind(1, code);
+
+        if (query.exec())
+        {
+            crow::json::wvalue x;
+            x["body"]["message"] = "Deleted";
+            return crow::response(x);
         }
-    });
+        else
+        {
+            return crow::response(400);
+        } });
 
+    CROW_ROUTE(app, "/api/url/<string>").methods(crow::HTTPMethod::PUT)([](const std::string code)
+                                                                        {
+        SQLite::Database db("test.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+        SQLite::Statement query(db, "UPDATE urls SET long_url = ? WHERE code = ?");
+        query.bind(1, code);
 
-    //set the port, set the app to run on multiple threads, and run the app
-    app.port(18080).multithreaded().run();
+        if (query.exec())
+        {
+            crow::json::wvalue x;
+            x["body"]["message"] = "Updated";
+            return crow::response(x);
+        }
+        else
+        {
+            return crow::response(400);
+        } });
 
+    CROW_ROUTE(app, "/api/url/<string>").methods(crow::HTTPMethod::PATCH)([](const std::string code)
+                                                                          {
+        SQLite::Database db("test.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+        SQLite::Statement query(db, "UPDATE urls SET long_url = ? WHERE code = ?");
+        query.bind(1, code);
+
+        if (query.exec())
+        {
+            crow::json::wvalue x;
+            x["body"]["message"] = "Updated";
+            return crow::response(x);
+        }
+        else
+        {
+            return crow::response(400);
+        } });
+
+    // set the port, set the app to run on multiple threads, and run the app
+    app.port(8000)
+        .multithreaded()
+        .run();
 }
 
 // TODO router like i did with express that can be funny
