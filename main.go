578e97843a3c6cb3088e41eea4bd2d8ca9dfc4ae
Ackimixs_ <ackimixs@protonmail.com>
2023-10-05 21:56:55 +0200

    update

--- main.go
+++ main.go
@@ -17,6 +17,12 @@ type Url struct {
 	Code    string `json:"code"`
 }
 
+type UrlId struct {
+	Id      int    `json:"id"`
+	LongUrl string `json:"long_url"`
+	Code    string `json:"code"`
+}
+
 func main() {
 
 	fmt.Println("Hello, world!")
@@ -39,7 +45,7 @@ func main() {
 
 	sqlStmt := `
 	DROP TABLE IF EXISTS url;
-	create table url (long_url text not null primary key, code text not null);
+	create table url (id INTEGER PRIMARY KEY AUTOINCREMENT, long_url text not null, code text not null);
 	delete from url;
 	`
 	_, err = db.Exec(sqlStmt)
@@ -67,139 +73,29 @@ func main() {
 	})
 
 	router.GET("/api/url", func(c *gin.Context) {
-		code := c.Query("code")
-		long_url := c.Query("long_url")
-
-		if code == "" && long_url == "" {
-			rows, err := db.Query("select long_url, code from url")
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			defer rows.Close()
-			var urls []Url
-			for rows.Next() {
-				var url Url
-				err = rows.Scan(&url.LongUrl, &url.Code)
-				if err != nil {
-					log.Fatal(err)
-					c.JSON(http.StatusNotFound, gin.H{
-						"message": "Not found",
-					})
-				}
-				urls = append(urls, url)
-			}
-			err = rows.Err()
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			c.JSON(http.StatusOK, gin.H{
-				"body": gin.H{
-					"url": urls,
-				},
-			})
-
-			return
-		} else if code == "" {
-			rows, err := db.Query("select long_url, code from url where long_url = ?", long_url)
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			defer rows.Close()
-			var url Url
-			for rows.Next() {
-				err = rows.Scan(&url.LongUrl, &url.Code)
-				if err != nil {
-					log.Fatal(err)
-					c.JSON(http.StatusNotFound, gin.H{
-						"message": "Not found",
-					})
-				}
-			}
-			err = rows.Err()
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			c.JSON(http.StatusOK, gin.H{
-				"body": gin.H{
-					"url": []Url{url},
-				},
-			})
-			c.JSON(http.StatusNotFound, gin.H{
-				"message": "Not found",
-			})
-			return
-		} else if long_url == "" {
-			rows, err := db.Query("select long_url, code from url where code = ?", code)
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			defer rows.Close()
-			var url Url
-			for rows.Next() {
-				err = rows.Scan(&url.LongUrl, &url.Code)
-				if err != nil {
-					log.Fatal(err)
-					c.JSON(http.StatusNotFound, gin.H{
-						"message": "Not found",
-					})
-				}
-			}
-			err = rows.Err()
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			c.JSON(http.StatusOK, gin.H{
-				"body": gin.H{
-					"url": []Url{url},
-				},
-			})
-			c.JSON(http.StatusNotFound, gin.H{
-				"message": "Not found",
-			})
-			return
-		} else {
-			var url Url
-			rows, err := db.Query("select long_url, code from url where code = ? && long_url = ?", code, long_url)
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			defer rows.Close()
-			rows.Next()
-			err = rows.Scan(&url.LongUrl, &url.Code)
-			if err != nil {
-				log.Fatal(err)
-				c.JSON(http.StatusNotFound, gin.H{
-					"message": "Not found",
-				})
-			}
-			c.JSON(http.StatusOK, gin.H{
-				"body": gin.H{
-					"url": []Url{url},
-				},
-			})
-			return
+		rows, err := db.Query("select * from url")
+		if err != nil {
+			log.Fatal(err)
 		}
+		defer rows.Close()
+		var urls []UrlId
+		for rows.Next() {
+			var url UrlId
+			err = rows.Scan(&url.Id, &url.LongUrl, &url.Code)
+			if err != nil {
+				log.Fatal(err)
+			}
+			urls = append(urls, url)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"body": gin.H{
+				"url": urls,
+			},
+		})
 	})
 
 	router.POST("/api/url", func(c *gin.Context) {
@@ -210,17 +106,87 @@ func main() {
 			return
 		}
 
-		// post in the database
+		res, err := db.Exec("insert into url (long_url, code) values (?, ?) RETURNING *", body.LongUrl, body.Code)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := res.LastInsertId()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		url := getUrl(fmt.Sprintf("%d", id), db)
+
+		c.JSON(http.StatusAccepted, gin.H{
+			"body": gin.H{
+				"url": UrlId{
+					Id:      int(id),
+					LongUrl: url.LongUrl,
+					Code:    url.Code,
+				},
+			},
+		})
+	})
+
+	router.GET("/api/url/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		rows, err := db.Query("select long_url, code from url where id = ?", id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		var url Url
+		for rows.Next() {
+			err = rows.Scan(&url.LongUrl, &url.Code)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"body": gin.H{
+				"url": url,
+			},
+		})
+	})
+
+	router.PUT("/api/url/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		body := Url{}
+
+		if err := c.BindJSON(&body); err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		url := getUrl(id, db)
+		if body.LongUrl == "" {
+			body.LongUrl = url.LongUrl
+		}
+		if body.Code == "" {
+			body.Code = url.Code
+		}
+
+		println(body.Code, body.LongUrl)
+
+		// update in the database
 		tx, err := db.Begin()
 		if err != nil {
 			log.Fatal(err)
 		}
-		stmt, err := tx.Prepare("insert into url(long_url, code) values(?, ?)")
+		stmt, err := tx.Prepare("update url set long_url = ?, code = ? where id = ?")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer stmt.Close()
-		_, err = stmt.Exec(body.LongUrl, body.Code)
+		_, err = stmt.Exec(body.LongUrl, body.Code, id)
 		err = tx.Commit()
 		if err != nil {
 			log.Fatal(err)
@@ -228,7 +194,32 @@ func main() {
 
 		c.JSON(http.StatusAccepted, gin.H{
 			"body": gin.H{
-				"url": body,
+				"url": getUrl(id, db),
+			},
+		})
+	})
+
+	router.DELETE("/api/url/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		// delete in the database
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+		stmt, err := tx.Prepare("delete from url where id = ?")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+		_, err = stmt.Exec(id)
+		err = tx.Commit()
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusAccepted, gin.H{
+			"body": gin.H{
+				"url": "deleted",
 			},
 		})
 	})
@@ -264,5 +255,27 @@ func main() {
 		c.Redirect(http.StatusMovedPermanently, url.LongUrl)
 	})
 
-	router.Run("localhost:8080")
+	router.Run("localhost:8000")
+}
+
+func getUrl(id string, db *sql.DB) UrlId {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare("select * from url where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var url UrlId
+	err = stmt.QueryRow(id).Scan(&url.Id, &url.LongUrl, &url.Code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return url
 }
